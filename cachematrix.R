2b35ed7369acef6581c0172b2c203256100ea1e3
Carlos Mantilla <cfmantilla@gmail.com>
2014-04-17 16:38:35 -0500

    modified functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions create an object that stores a matrix, calculate 
+## its inverse and cache the result.
+## They use the same logic as the makeVector and cachemean functions 
+## in the coursera rprog-002 course examples
 
-## Write a short comment describing this function
 
+## This function returns a list of functions (set,get,getsolved,getsolved),
+## as a side effect it updates the variable x in the parent environment 
+## when functions set or setsolved are used
 makeCacheMatrix <- function(x = matrix()) {
-
+  solved <- NULL
+  set <- function(y) {
+    x <<- y
+    solved <<- NULL
+  }
+  get <- function() x
+  setsolved <- function(inverse = matrix()) solved <<- inverse
+  getsolved <- function() solved
+  list(set = set, get = get,
+       setsolved = setsolved,
+       getsolved = getsolved) 
 }
 
-
-## Write a short comment describing this function
-
+## This functions returns the inverse matrix of x, if the inverse 
+## has been calculated before it returns its stored value, if
+## there is no cached value it calculates the inverse and keeps
+## the result to avoid having to recalculate
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  solved <- x$getsolved()
+  if(!is.null(solved)) {
+    message("getting cached data")
+    return(solved)
+  }
+  data <- x$get()
+  solved <- solve(data, ...)
+  x$setsolved(solved)
+  solved  
 }
